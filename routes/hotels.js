0c2299b8b31f543a497d475fb1a08f39ea647a3f
deeiip <ping@dipanjan.me>
2016-05-08 14:03:01 +0530

    user and admin authority with session key working corrently

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,10 +1,19 @@
 var express = require('express');
-var auth = require('../middlewares/UserAuth')
+var auth = require('../middlewares/UserAuth');
+var adminAuth = require('../middlewares/AdminAuth');
+var Hotel = require('../models/Hotels');
 var router = express.Router();
 
 /* GET hotel listing. Require user auth */
-router.get('/',auth ,  function(req, res) {
+router.get('/:city_code/:date',auth ,  function(req, res) {
   res.send('respond with a resource');
+});
+
+/// Create a new hotel. Requires admin auth
+router.route('/:name').post(adminAuth,function(req, res) {
+  //Hotel.create({
+		res.send('respond with a resource');
+	//});
 });
 
 module.exports = router;
