4bdfc93b6ac9bb374e47126cd2ad78275e2f9f01
Dipanjan Nag <ping@dipanjan.me>
2017-01-13 20:44:00 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,4 +1,4 @@
-var db_str = process.env.DB_STR || 'mysql://root:acdh3546@sledgehammer.ctnkwcpvczis.us-west-2.rds.amazonaws.com/shortrip_alpha?reconnect=true';
+var db_str = process.env.DB_STR || 'mysql://admin:acdh3546@shortrip.c5e0mzj26vsz.us-west-2.rds.amazonaws.com:3306/shortrip_alpha?reconnect=true';
 module.exports = {
     connection_str : db_str
 };
